a6ed474870789087bfb0b82d4a34c79354a301e5
João Igor de Sousa Ferro <joaoigorsf990@gmail.com>
2025-12-14 19:47:51 -0300

    correção dos metodos setters

--- RestauranteJava/src/classes/Prato.java
+++ RestauranteJava/src/classes/Prato.java
@@ -1,3 +1,4 @@
+package Classes;
 
 public class Prato {
     private int codigo;
@@ -6,73 +7,74 @@ public class Prato {
     private double preco;
     private boolean disp;
     private String descricao;
-            
-    public Prato(String nome, String categoria, double preco, boolean disponibilidade, String descricao){
+
+    public Prato(String nome, String categoria, double preco, boolean disp, String descricao) {
         this.nome = nome;
         this.categoria = categoria;
         this.preco = preco;
         this.disp = disp;
         this.descricao = descricao;
     }
-    
-    public Prato(){
+
+    public Prato() {
     }
-    
-    public int getCod(){
+
+    public int getCodigo() {
         return codigo;
     }
-    
-    public void setCod(int codigo){
+
+    public void setCodigo(int codigo) {
         this.codigo = codigo;
     }
-    
-    public String getNome(){
+
+    public String getNome() {
         return nome;
     }
-    
-    public void setNome(){
+
+    public void setNome(String nome) {
         this.nome = nome;
     }
-    
-    public String getCategoria(){
-        return descricao;
+
+    public String getCategoria() {
+        return categoria;
     }
-    
-    public void setCategoria(){
+
+    public void setCategoria(String categoria) {
         this.categoria = categoria;
     }
-    
-    public double getPreco(){
+
+    public double getPreco() {
         return preco;
     }
-    
-    public void setPreco(){
+
+    public void setPreco(double preco) {
         this.preco = preco;
     }
-    
-    public boolean getDisp(){
+
+    public boolean getDisp() {
         return disp;
     }
-    
-    public void setDisp(){
+
+    public void setDisp(boolean disp) {
         this.disp = disp;
     }
-    
-    public String getDescricao(){
+
+    public String getDescricao() {
         return descricao;
     }
-    
-    public void setDescricao(){
+
+    public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
-    
+
     @Override
-    public String toString(){
+    public String toString() {
         return "Código do Prato: " + codigo + "\n" +
-                "Nome: " + nome + "\n" +
-                "Categoria: " + categoria + "\n" +
-                "Preço: " + preco + "\n" +
-                "Disponibilidade: " + disp + "\n" +
-                "Descrição: " + descricao + "\n";
+               "Nome: " + nome + "\n" +
+               "Categoria: " + categoria + "\n" +
+               "Preço: " + preco + "\n" +
+               "Disponibilidade: " + disp + "\n" +
+               "Descrição: " + descricao + "\n";
     }
 }
+
