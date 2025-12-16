d4d7ca922f82982aa0c35aa54b5be36e2a696547
José Gabriel Soares dos Santos <josgabrielsoares825@gmail.com>
2025-12-16 11:03:32 -0300

    Update Main.java

--- RestauranteJava/src/classes/Main.java
+++ RestauranteJava/src/classes/Main.java
@@ -1,12 +1,34 @@
-package main;
+package app;
 
-import formulario.FrmMenu;
+import formularios.FrmRestaurante;
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        /* Define o Look and Feel (Nimbus) */
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(
+                null,
+                "Erro ao carregar o tema visual do sistema.\n"
+              + "O sistema será iniciado com o tema padrão.",
+                "Aviso",
+                JOptionPane.WARNING_MESSAGE
+            );
+        }
+
+        /* Inicia a aplicação pelo formulário principal */
         java.awt.EventQueue.invokeLater(() -> {
-            new FrmMenu().setVisible(true);
+            new FrmRestaurante().setVisible(true);
         });
     }
 }
